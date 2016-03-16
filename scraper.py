e9c571ecff27a7334fbfdc0db82b6a9521bb8d67
erinclark <Mtkm2hds>
2016-03-16 13:35:01 +0000

    test

--- scraper.py
+++ scraper.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 from bs4 import BeautifulSoup
-from splinter import Browser
-import sys
 import urllib
-import time
 from datetime import datetime
 import scraperwiki
 
@@ -39,24 +36,103 @@ def get_links(url):
     return links
 
 
+def get_title(soup):
+    title = soup.find('div', id="container")\
+        .findNext('h2').text.strip().encode('utf-8')
+    return title
+
+
+def get_info(soup, text):
+    try:
+        info = soup.find('th', text=text)\
+            .findNext('td').text.strip().encode('utf-8')
+    except:
+        info = ''
+    return info
+
+
+
 if __name__ == '__main__':
 
     todays_date = str(datetime.now())
-    portals = ['https://www.tenders.gov.au/?startRow=0&event=public%2EATM%2Elist',
-               'https://www.tenders.gov.au/?startRow=0&event=public%2EATM%2Eclosed']
+    portals = [['open', 'https://www.tenders.gov.au/?startRow=0&event=public%2EATM%2Elist'],
+               ['closed', 'https://www.tenders.gov.au/?startRow=0&event=public%2EATM%2Eclosed']]
+    errors = []
 
-    for p in portals:
+    for sp in portals:
 
-        last_page = get_pages(p)
-        for p_num in range(0, last_page):
+        p = sp[1]
+        try:
+
+            last_page = get_pages(p)
+            for p_num in range(0, last_page):
+
+                try:
+                    print p_num
+                    page = p[:p.find('Row=')+4] + str(p_num*15) + p[p.find('&event'):]
+                    print page
+                    links = get_links(page)
+                except Exception as e:
+                    errors.append([page, e])
+                    continue
 
 
-            page = p[:p.find('Row=')+4] + str(p_num*15) + p[p.find('&event'):]
-            print page
-            links = get_links(page)
-            for link in links:
-                print link
+                for link in links:
+                    try:
+                        print link
+                        tender_soup = get_soup(link)
+                        status = sp[0]
+                        tender_url = link
+                        uuid = link[link.find('UUID=')+5:]
+                        title = get_title(tender_soup)
+                        amt_id = get_info(tender_soup, 'ATM ID')
+                        agency = get_info(tender_soup, 'Agency')
+                        category = get_info(tender_soup, 'Category')
+                        deadline = get_info(tender_soup, 'Close Date & Time')
+                        publish_date = get_info(tender_soup, 'Publish Date')
+                        location = get_info(tender_soup, 'Location')
+                        multi_agency_access= get_info(tender_soup, 'Multi Agency Access')
+                        panel_arrangement = get_info(tender_soup, 'Panel Arrangement')
+                        description = get_info(tender_soup, 'Description')
+                        other_instructions = get_info(tender_soup, 'Other Instructions ')
+                        conditions_for_participation = get_info(tender_soup, 'Conditions for Participation')
+                        delivery_timeframe = get_info(tender_soup, 'Timeframe for Delivery')
+                        lodgement_address = get_info(tender_soup, 'Address for Lodgement')
+                        #= 'Addenda Available')   #TODO only with login
+                        contact_officer = get_info(tender_soup, 'Contact Officer')
+                        phone = get_info(tender_soup, 'Phone Number')
+                        email = get_info(tender_soup, 'Email Address')
 
 
+                        data = {"tender_url":unicode(tender_url),
+                                "status": unicode(status),
+                                "uuid": unicode(uuid),
+                                "title": unicode(title),
+                                "amt_id": unicode(amt_id),
+                                "agency": unicode(agency),
+                                "category": unicode(category),
+                                "deadline": unicode(deadline),
+                                "publish_date": unicode(publish_date),
+                                "location": unicode(location),
+                                "multi_agency_access": unicode(multi_agency_access),
+                                "panel_arrangement": unicode(panel_arrangement),
+                                "description": unicode(description),
+                                "other_instructions": unicode(other_instructions),
+                                "conditions_for_participation": unicode(conditions_for_participation),
+                                "delivery_timeframe": unicode(delivery_timeframe),
+                                "lodgement_address": unicode(lodgement_address),
+                                "contact_officer": unicode(contact_officer),
+                                "phone": unicode(phone),
+                                "email": unicode(email),
+                                "date": todays_date}
+
+                        scraperwiki.sqlite.save(unique_keys=['tender_url'], data=data)
+
+                    except Exception as e:
+                        errors.append([link, e])
+        except KeyboardInterrupt:
+            sys.exit("forced exit")
 
 
+    print 'No. errors ', len(errors)
+    print errors
